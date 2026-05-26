0d1b51893337ec1b679895f41409d1902284c669
Erick Fellipe <efrc22br@gmail.com>
2026-05-26 14:52:48 -0300

    Corrigido erro de sincronização de dados

--- src/schemas/itemSchema.ts
+++ src/schemas/itemSchema.ts
@@ -23,7 +23,7 @@ const epicWeaponSchema = z.object({
   id: z.number(),
   name: z.string(),
   weight: z.number(),
-  rarity: z.literal("Common"),
+  rarity: z.literal("Epic"),
   damage: z.number().min(25).max(35),
   bonus: z.number().min(0.1).max(1),
   itemType: z.literal("Weapon"),
@@ -32,7 +32,8 @@ const epicWeaponSchema = z.object({
 const mythicWeaponSchema = z.object({
   id: z.number(),
   name: z.string(),
-  rarity: z.literal("Common"),
+  weight: z.number(),
+  rarity: z.literal("Mythic"),
   damage: z.number().min(36).max(40),
   bonus: z.number().min(0.5).max(1.5),
   itemType: z.literal("Weapon"),
@@ -42,20 +43,12 @@ const legendaryWeaponSchema = z.object({
   id: z.number(),
   name: z.string(),
   weight: z.number(),
-  rarity: z.literal("Common"),
+  rarity: z.literal("Legendary"),
   damage: z.number().min(40).max(50),
   bonus: z.number().min(1).max(2),
   specialAttack: z.string(),
   itemType: z.literal("Weapon"),
 });
-
-export const weaponsSchema = z.discriminatedUnion("rarity", [
-  commonWeaponSchema,
-  rareWeaponSchema,
-  epicWeaponSchema,
-  mythicWeaponSchema,
-  legendaryWeaponSchema,
-]);
 
 //SCHEMAS DE ARMADURAS
 
@@ -112,14 +105,6 @@ const legendaryArmorSchema = z.object({
   itemType: z.literal("Armor"),
 });
 
-export const armorSchema = z.discriminatedUnion("rarity", [
-  commonArmorSchema,
-  rareArmorSchema,
-  epicArmorSchema,
-  mythicArmorSchema,
-  legendaryArmorSchema,
-]);
-
 //SCHEMAS DE CONSUMÍVEIS
 
 const lifePotionSchema = z.object({
@@ -154,25 +139,27 @@ const strengthPotionSchema = z.object({
   name: z.string(),
   weight: z.number(),
   type: z.literal("strength"),
-  manaToRestore: z.number().min(1).max(100),
   strengthToIncrease: z.number().min(2).max(5),
   itemType: z.literal("Consumable"),
 });
 
-export const potionsSchema = z.discriminatedUnion("type", [
+export const itemsSchema = z.union([
+  commonWeaponSchema,
+  rareWeaponSchema,
+  epicWeaponSchema,
+  mythicWeaponSchema,
+  legendaryWeaponSchema,
+
+  commonArmorSchema,
+  rareArmorSchema,
+  epicArmorSchema,
+  mythicArmorSchema,
+  legendaryArmorSchema,
+
   lifePotionSchema,
   manaPotionSchema,
   defensePotionSchema,
   strengthPotionSchema,
 ]);
 
-export const itemsSchema = z.discriminatedUnion("itemType", [
-  weaponsSchema,
-  armorSchema,
-  potionsSchema,
-]);
-
-export type items =
-  | z.infer<typeof weaponsSchema>
-  | z.infer<typeof armorSchema>
-  | z.infer<typeof potionsSchema>;
+export type items = z.infer<typeof itemsSchema>;
