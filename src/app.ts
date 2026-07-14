88716e98ef1f445cde6e7acbba9c8345bebcc246
Erick Fellipe <efrc22br@gmail.com>
2026-07-14 15:15:25 -0300

    Adicionado rota loot

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import characterRoute from "./routes/character-routes";
 import inventoryRoute from "./routes/inventory-routes";
 import itemRouter from "./routes/items-routes";
+import lootRouter from "./routes/loot-route";
 
 export default function createApp() {
   const app = express();
@@ -10,6 +11,7 @@ export default function createApp() {
   app.use("/rpgAPI", characterRoute);
   app.use("/rpgAPI", inventoryRoute);
   app.use("/rpgAPI", itemRouter);
+  app.use("/rpgAPI", lootRouter);
 
   return app;
 }
